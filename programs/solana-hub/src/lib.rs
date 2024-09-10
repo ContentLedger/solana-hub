730d8c54f2c13fbb0dd20df6544c3d22e03aae5c
Rodrigo Pintos <rpintos@spacedev.io>
2024-09-10 00:38:06 -0300

    working in the claim

--- programs/solana-hub/src/lib.rs
+++ programs/solana-hub/src/lib.rs
@@ -1,9 +1,12 @@
 use anchor_lang::prelude::*;
-
-// use anchor_spl::{
-//     associated_token::AssociatedToken,
-//     token::{Mint, Token, TokenAccount},
-// };
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    metadata::{
+        create_metadata_accounts_v3, mpl_token_metadata::types::DataV2, CreateMetadataAccountsV3,
+        Metadata as Metaplex,
+    },
+    token::{mint_to, Mint, MintTo, Token, TokenAccount},
+};
 
 declare_id!("EsE5KPaDVc5wrrmJSDCUbaWSds35x56A3XkDUKkexHb6");
 
@@ -16,8 +19,8 @@ pub mod solana_hub {
     pub fn register_collection(
         ctx: Context<RegisterCollection>,
         _name: String,
-        _quantity: u16,
         seconds_to_close: u64,
+        nft_list: Vec<String>,
     ) -> Result<()> {
         let get_clock = Clock::get();
         if let Ok(clock) = get_clock {
@@ -25,7 +28,8 @@ pub mod solana_hub {
             ctx.accounts.auction.creator = ctx.accounts.creator.key();
             let timestamp_to_close = current_timestamp.add(seconds_to_close as i64);
             ctx.accounts.auction.timestamp_to_close = timestamp_to_close;
-            msg!("{0} and {1}", current_timestamp, timestamp_to_close,)
+            ctx.accounts.auction.nft_list = nft_list;
+            msg!("{0} and {1}", current_timestamp, timestamp_to_close,);
         }
         Ok(())
     }
@@ -49,25 +53,73 @@ pub mod solana_hub {
     // whether the NFT is claimed.
     pub fn claim(ctx: Context<Claim>, name: String, nft_id: u16) -> Result<()> {
         //TODO: claim only if timestamp_to_close
-        msg!(
-            "{0},{1},{2}",
-            ctx.accounts.auction.key(),
-            name.to_string(),
-            nft_id.to_string()
+
+        if nft_id as usize > ctx.accounts.auction.nft_list.len() {
+            return Ok(()); //TODO: Throw error
+        }
+
+        let uri = ctx
+            .accounts
+            .auction
+            .nft_list
+            .get(nft_id as usize - 1)
+            .unwrap()
+            .to_string();
+
+        let nft_data: DataV2 = DataV2 {
+            name: name.clone(),
+            symbol: String::from("PRB"), //TODO: Change this with metadata.name
+            uri,                         //TODO: Change this with metadata.symbol
+            seller_fee_basis_points: 3,
+            creators: None,   //TODO: [ctx.accounts.auction.creator.key()],
+            collection: None, //TODO: See how to put all the nfts under the same collection
+            uses: None,
+        };
+
+        let nft_signer_seeds = &["nft".as_bytes(), name.as_bytes(), &nft_id.to_be_bytes()];
+        let nft_signer = [&nft_signer_seeds[..]];
+        let metadata_ctx = CpiContext::new_with_signer(
+            ctx.accounts.token_metadata_program.to_account_info(),
+            CreateMetadataAccountsV3 {
+                payer: ctx.accounts.claimer.to_account_info(),
+                update_authority: ctx.accounts.nft.to_account_info(),
+                mint: ctx.accounts.nft.to_account_info(),
+                metadata: ctx.accounts.metadata.to_account_info(),
+                mint_authority: ctx.accounts.nft.to_account_info(),
+                system_program: ctx.accounts.system_program.to_account_info(),
+                rent: ctx.accounts.rent.to_account_info(),
+            },
+            &nft_signer,
         );
+
+        mint_to(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info(),
+                MintTo {
+                    authority: ctx.accounts.nft.to_account_info(),
+                    to: ctx.accounts.claimer.to_account_info(),
+                    mint: ctx.accounts.nft.to_account_info(),
+                },
+                &nft_signer,
+            ),
+            1,
+        )?;
+
+        create_metadata_accounts_v3(metadata_ctx, nft_data, false, true, None)?;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-#[instruction(name:String, quantity:u16, minutes_to_close:u64)]
+#[instruction(name:String, seconds_to_close:u64, nft_list:Vec<String>)]
 pub struct RegisterCollection<'info> {
     #[account(
         init,
         seeds = ["auction".as_bytes(),name.as_bytes()],
         bump,
         payer = creator,
-        space = 8+32+8
+        space = Auction::MAX_SIZE
     )]
     pub auction: Account<'info, Auction>,
     #[account(mut)]
@@ -85,10 +137,10 @@ pub struct Bid<'info> {
     pub auction: Account<'info, Auction>,
     #[account(
         init_if_needed,
-        seeds = ["nft-auction".as_bytes(),name.as_bytes(),&nft_id.to_be_bytes()],
+        seeds = ["nft_auction".as_bytes(),name.as_bytes(),&nft_id.to_be_bytes()],
         bump,
         payer = bidder,
-        space = 8+2+32+8
+        space = NftAuction::MAX_SIZE,
     )]
     pub nft_auction: Account<'info, NftAuction>,
     #[account(mut)]
@@ -99,30 +151,47 @@ pub struct Bid<'info> {
 #[derive(Accounts)]
 #[instruction(name:String, nft_id:u16)]
 pub struct Claim<'info> {
+    /// CHECK: New Metaplex Account being created
+    #[account(mut)]
+    pub metadata: UncheckedAccount<'info>,
     #[account(
         seeds = ["auction".as_bytes(),name.as_bytes()],
         bump
     )]
     pub auction: Account<'info, Auction>,
     #[account(
-        seeds = ["nft-auction".as_bytes(),name.as_bytes(),&nft_id.to_be_bytes()],
+        init_if_needed,//this because maybe none bid but we need to check into it
+        seeds = ["nft_auction".as_bytes(),name.as_bytes(),&nft_id.to_be_bytes()],
         bump,
+        payer=claimer,
+        space = NftAuction::MAX_SIZE,
+        //TODO: Uncomment this
+        //constraint = nft_auction.bidder.key() == claimer.key()
     )]
     pub nft_auction: Account<'info, NftAuction>,
-    // #[account(mut)]
-    // pub token: Account<'info, Mint>,
-    // #[account(
-    //     init,
-    //     payer = claimer,
-    //     associated_token::mint = token,
-    //     associated_token::authority = claimer,
-    // )]
-    // pub receiver_account: Account<'info, TokenAccount>,
+    #[account(
+        init,
+        seeds = ["nft".as_bytes(),name.as_bytes(),&nft_id.to_be_bytes()],
+        bump,
+        payer = claimer,
+        mint::decimals = 0,
+        mint::authority = nft,
+    )]
+    pub nft: Account<'info, Mint>,
+    #[account(
+        init,
+        payer = claimer,
+        associated_token::mint = nft,
+        associated_token::authority = claimer,
+    )]
+    pub receiver_account: Account<'info, TokenAccount>,
     #[account(mut)]
     pub claimer: Signer<'info>,
+    pub rent: Sysvar<'info, Rent>,
     pub system_program: Program<'info, System>,
-    // pub token_program: Program<'info, Token>,
-    //pub associated_token_program: Program<'info, AssociatedToken>,
+    pub token_program: Program<'info, Token>,
+    pub token_metadata_program: Program<'info, Metaplex>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
 }
 
 #[account]
@@ -130,6 +199,13 @@ pub struct Claim<'info> {
 pub struct Auction {
     pub creator: Pubkey,
     pub timestamp_to_close: i64,
+    pub nft_list: Vec<String>, //TODO: Vec<struct with metadata (uri, name, symbol)>
+}
+
+impl Auction {
+    //up to 10 nfts
+    //length nft_list string up to 63
+    pub const MAX_SIZE: usize = 8 + 32 + 8 + (4 + 10 * (4 + 63));
 }
 
 #[account]
@@ -138,4 +214,8 @@ pub struct NftAuction {
     pub nft_id: u16,
     pub bidder: Pubkey,
     pub price: u64, //in lamports
+}
+
+impl NftAuction {
+    pub const MAX_SIZE: usize = 8 + 2 + 32 + 8;
 }
--- tests/solana-hub.ts
+++ tests/solana-hub.ts
@@ -10,7 +10,19 @@ const accountWithLamports = anchor.web3.Keypair.fromSecretKey(
   )
 );
 
-const COLLECTION_NAME = "La Piedra Filosofal 6";
+const COLLECTION_NAME = "La Piedra Filosofal 10";
+const NFT_LIST = [
+  "https://arweave.net/trLCtzS7x9YlA3cpwCIdugdrEYghgT6mQEM7hmZDcA4",
+  "https://arweave.net/trLCtzS7x9YlA3cpwCIdugdrEYghgT6mQEM7hmZDcA4",
+  "https://arweave.net/trLCtzS7x9YlA3cpwCIdugdrEYghgT6mQEM7hmZDcA4",
+  "https://arweave.net/trLCtzS7x9YlA3cpwCIdugdrEYghgT6mQEM7hmZDcA4",
+  "https://arweave.net/trLCtzS7x9YlA3cpwCIdugdrEYghgT6mQEM7hmZDcA4",
+  "https://arweave.net/trLCtzS7x9YlA3cpwCIdugdrEYghgT6mQEM7hmZDcA4",
+  "https://arweave.net/trLCtzS7x9YlA3cpwCIdugdrEYghgT6mQEM7hmZDcA4",
+  "https://arweave.net/trLCtzS7x9YlA3cpwCIdugdrEYghgT6mQEM7hmZDcA4",
+  "https://arweave.net/trLCtzS7x9YlA3cpwCIdugdrEYghgT6mQEM7hmZDcA4",
+  "https://arweave.net/trLCtzS7x9YlA3cpwCIdugdrEYghgT6mQEM7hmZDcA4",
+];
 
 const predictAuction = (program: Program<SolanaHub>, name: string) => {
   const SOLANA_HUB_PROGRAM_ID = new anchor.web3.PublicKey(program.programId);
@@ -19,6 +31,40 @@ const predictAuction = (program: Program<SolanaHub>, name: string) => {
     SOLANA_HUB_PROGRAM_ID
   );
   return auction;
+};
+
+const predictMetadataAccount = (
+  program: Program<SolanaHub>,
+  name: string,
+  nftId: number
+) => {
+  const METADATA_SEED = "metadata";
+  const TOKEN_METADATA_PROGRAM_ID = new anchor.web3.PublicKey(
+    "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
+  );
+  const [metadataAddress] = anchor.web3.PublicKey.findProgramAddressSync(
+    [
+      Buffer.from(METADATA_SEED),
+      TOKEN_METADATA_PROGRAM_ID.toBuffer(),
+      predictNft(program, name, nftId).toBuffer(),
+    ],
+    TOKEN_METADATA_PROGRAM_ID
+  );
+
+  return metadataAddress;
+};
+
+const predictNft = (
+  program: Program<SolanaHub>,
+  name: string,
+  nftId: number
+) => {
+  const SOLANA_HUB_PROGRAM_ID = new anchor.web3.PublicKey(program.programId);
+  const [nft] = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("nft"), Buffer.from(name), u16ToBigEndianBuffer(nftId)],
+    SOLANA_HUB_PROGRAM_ID
+  );
+  return nft;
 };
 
 function u16ToBigEndianBuffer(num: number): Buffer {
@@ -46,11 +92,43 @@ describe("solana-hub", () => {
 
   const program = anchor.workspace.SolanaHub as Program<SolanaHub>;
 
-  it("Register collection!", async () => {
+  // it("Register collection!", async () => {
+  //   // Add your test here.
+  //   const txInstruction = await program.methods
+  //     .registerCollection(COLLECTION_NAME, new anchor.BN(5 * 60), NFT_LIST)
+  //     .accounts({ creator: accountWithLamports.publicKey })
+  //     .instruction();
+  //   const transaction = new anchor.web3.Transaction().add(txInstruction);
+  //   transaction.recentBlockhash = (
+  //     await program.provider.connection.getLatestBlockhash("finalized")
+  //   ).blockhash;
+  //   transaction.sign(accountWithLamports);
+  //   const txHash = await program.provider.connection.sendRawTransaction(
+  //     transaction.serialize(),
+  //     { preflightCommitment: "confirmed" }
+  //   );
+  //   const confirmation = await program.provider.connection.confirmTransaction(
+  //     txHash,
+  //     "confirmed"
+  //   );
+  //   if (!confirmation.value.err) {
+  //     console.log("Your transaction signature", txHash);
+  //     const auctionAccount = predictAuction(program, COLLECTION_NAME);
+  //     const accountInfo = await program.account.auction.fetch(auctionAccount);
+  //     console.log(accountInfo.timestampToClose.toNumber());
+  //     console.log(accountInfo.nftList);
+  //   }
+  // });
+  it("Claim nft!", async () => {
     // Add your test here.
+    const nftId = 1;
+    console.log(predictNft(program, COLLECTION_NAME, nftId));
     const txInstruction = await program.methods
-      .registerCollection(COLLECTION_NAME, 10, new anchor.BN(5 * 60))
-      .accounts({ creator: accountWithLamports.publicKey })
+      .claim(COLLECTION_NAME, nftId)
+      .accounts({
+        metadata: predictMetadataAccount(program, COLLECTION_NAME, nftId),
+        claimer: accountWithLamports.publicKey,
+      })
       .instruction();
     const transaction = new anchor.web3.Transaction().add(txInstruction);
     transaction.recentBlockhash = (
@@ -61,9 +139,14 @@ describe("solana-hub", () => {
       transaction.serialize(),
       { preflightCommitment: "confirmed" }
     );
-    console.log("Your transaction signature", txHash);
-    // const auctionAccount = predictAuction(program, COLLECTION_NAME);
-    // const accountInfo = await program.account.auction.fetch(auctionAccount);
-    // console.log(accountInfo.timestampToClose.toNumber());
+    const confirmation = await program.provider.connection.confirmTransaction(
+      txHash,
+      "confirmed"
+    );
+    if (!confirmation.value.err) {
+      console.log("Your transaction signature", txHash);
+      const nft = predictNft(program, COLLECTION_NAME, nftId);
+      console.log(nft.toBase58());
+    }
   });
 });
