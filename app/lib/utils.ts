b40855bcfe1675bc40aab1f8a4e6a87c76c6e74f
Justin Walsh <contact.me@thejustinwalsh.com>
2024-09-11 22:14:47 -0400

    feat: pluralize helper

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -37,3 +37,17 @@ export function formatFileNames(
 
   return { imageFileName, metadataFileName };
 }
+
+const pluralRules = new Intl.PluralRules("en-US");
+
+export function pluralize(count: number, singular: string, plural: string) {
+  const grammaticalNumber = pluralRules.select(count);
+  switch (grammaticalNumber) {
+    case "one":
+      return count + " " + singular;
+    case "other":
+      return count + " " + plural;
+    default:
+      throw new Error("Unknown: " + grammaticalNumber);
+  }
+}
